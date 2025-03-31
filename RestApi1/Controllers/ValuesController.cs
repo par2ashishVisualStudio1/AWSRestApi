fc968f2dddf3684540cdb41bcf8f10dc1bf2e13f
par2ashish <par2ashish@gmail.com>
2025-03-31 12:06:54 +0530

    Add project files.

---
+++ RestApi1/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet(Name = "GetValues")]
+        public IEnumerable<string> Get()
+        {
+            return ["RestApi1_Value1", "RestApi1_Value2"];
+        }
+    }
+}
---
+++ RestApi2/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet(Name = "GetValues")]
+        public IEnumerable<string> Get()
+        {
+            return ["RestApi2_Value1", "RestApi2_Value2"];
+        }
+    }
+}
