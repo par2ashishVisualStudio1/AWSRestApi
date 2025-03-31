d3cb219dc47679141886188dec1206743bf2524a
par2ashish <par2ashish@gmail.com>
2025-03-31 23:05:55 +0530

    Added Lambda

--- RestApi1/Program.cs
+++ RestApi1/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
